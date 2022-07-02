ac828f78309be004cd3fe65438e5d5b3c0175506
talhea <talhea.seo@gmail.com>
2022-07-02 23:06:55 +0900

    down_opera 오류수정

--- down_opera.py
+++ down_opera.py
@@ -1,6 +1,6 @@
 import pyautogui as gui
 import datetime
-import os
+import os, shutil
 
 # 어제날짜 추출
 target_date = (datetime.datetime.now() - datetime.timedelta(1)).strftime('%Y%m%d')  # 어제날짜 이름
@@ -20,19 +20,20 @@ gui.sleep(5)
 # 로그인 창에서 로그인 클릭
 gui.moveTo(888, 595)
 gui.click()
-gui.sleep(5)
+gui.sleep(8)
 
 # Mescelleneouse 클릭
-gui.moveTo(1208, 183)
+gui.moveTo(1208, 183, duration=1)
 gui.click()
 
 # Reports 버튼 더블클릭
-gui.moveTo(365, 261)
-gui.doubleClick()
+gui.moveTo(365, 261, duration=1)
+gui.click()
 gui.sleep(2)
 
 # 검색창 입력
-gui.moveTo(725, 305)
+gui.moveTo(725, 305, duration=1)
+gui.click()
 gui.write('%trial', interval=0.4)
 gui.press('enter')
 
@@ -41,7 +42,7 @@ gui.moveTo(752, 673)
 gui.click()
 
 # file format 선택 : XML
-gui.moveTo(1027, 673)
+gui.moveTo(1027, 673, duration=1)
 gui.click()
 gui.click(1027, 750, duration=1)
 
@@ -51,29 +52,36 @@ gui.hotkey('alt', 'o')
 # file 이름 변경
 gui.hotkey('alt', 'f')
 gui.sleep(1)
-gui.moveTo(692, 546)
+gui.moveTo(692, 546, duration=1)
 gui.doubleClick()
 gui.sleep(1)
-date_str = 'trial_balance' + target_date
-gui.write(date_str, interval=0.4)
-gui.sleep(10)
 
-# 검색창 닫기
-gui.hotkey('alt' + 'c')
+target_file_name = 'trial_balance' + target_date
+gui.write(target_file_name, interval=0.4)
+gui.sleep(2)
+gui.moveTo(1005, 547, duration=1)
+gui.click()
+gui.sleep(2)
+
+# 검색창 닫기, 파일이 저장될 시간 필요
+# gui.hotkey('alt' + 'c')   # 이 때는 핫키가 안먹힘
+gui.moveTo(1090, 756)
+gui.click()
+gui.sleep(15)
 
 # 오페라 종료
-gui.moveTo(368, 175)
+gui.moveTo(368, 175, duration=1)
 gui.doubleClick()
+gui.sleep(8)
 
 # 최소화
-gui.sleep(2)
-gui.moveTo(1224, 17)
+gui.moveTo(1224, 17, duration=1)
 gui.click()
 
 gui.sleep(3)
 
 # 자료를 다운로드 받는 down디렉토리 생성
-down_base_dir = '.\\downdata\\' + target_date
+down_base_dir = './downdata/' + target_date
 try:
     if os.path.exists(down_base_dir) == False:
         os.makedirs(down_base_dir)
@@ -82,5 +90,7 @@ except:
         file.write(f'[down_opera.py] : {datetime.datetime.now()} - Exception: Cannot create the directory {down_base_dir}')
         print("Error: Cannot create the directory {}".format(down_base_dir))
 
-# 파일 옮기기 : 현재 디렉토리로
+# 파일 옮기기 : downdata디렉토리로
 # 디렉토리의 파일만 검색해서 .. 파일 이름에서 유추... 해당 파일을 복사/이동
+target_file_name = 'C:/users/fa2/' + target_file_name + '.xml'
+shutil.move(target_file_name, down_base_dir)
