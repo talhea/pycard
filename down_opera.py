9cc749de0efae8eea1aef29c375f22991f517413
talhea <talhea.seo@gmail.com>
2022-07-03 13:34:03 +0900

    down_opera 일부 수정

--- down_opera.py
+++ down_opera.py
@@ -3,7 +3,9 @@ import datetime
 import os, shutil
 
 # 어제날짜 추출
-target_date = (datetime.datetime.now() - datetime.timedelta(1)).strftime('%Y%m%d')  # 어제날짜 이름
+target_date = (datetime.datetime.now() - datetime.timedelta(1)).strftime(
+    "%Y%m%d"
+)  # 어제날짜 이름
 
 gui.PAUSE = 0.5
 gui.sleep(3)
@@ -12,54 +14,58 @@ gui.sleep(3)
 gui.moveTo(528, 876)
 gui.click()
 
-# 이미 로그인 된 상태이므로 PMS 버튼 클릭
+# 이미 로그인 된 상태이므로 PMS 버튼 클릭: PMS 로그인 화면 로딩
 gui.moveTo(769, 406)
 gui.click()
 gui.sleep(5)
 
-# 로그인 창에서 로그인 클릭
+# 로그인 창에서 로그인 클릭: PMS 화면 로딩
 gui.moveTo(888, 595)
 gui.click()
 gui.sleep(8)
 
 # Mescelleneouse 클릭
-gui.moveTo(1208, 183, duration=1)
+gui.moveTo(1208, 183)
 gui.click()
+gui.sleep(1)
 
-# Reports 버튼 더블클릭
-gui.moveTo(365, 261, duration=1)
+# Reports 버튼 클릭
+gui.moveTo(365, 261)
 gui.click()
 gui.sleep(2)
 
 # 검색창 입력
-gui.moveTo(725, 305, duration=1)
+gui.moveTo(725, 305)
 gui.click()
-gui.write('%trial', interval=0.4)
-gui.press('enter')
+gui.sleep(1)
+gui.write("%trial", interval=0.4)
+gui.press("enter")
+gui.sleep(1.5)
 
 # trial_balance가 선택된 상태이므로 파일 체크 시작
 gui.moveTo(752, 673)
 gui.click()
 
 # file format 선택 : XML
-gui.moveTo(1027, 673, duration=1)
+gui.moveTo(1027, 673)
 gui.click()
 gui.click(1027, 750, duration=1)
 
 # OK버튼 핫키
-gui.hotkey('alt', 'o')
+gui.hotkey("alt", "o")
 
 # file 이름 변경
-gui.hotkey('alt', 'f')
+gui.hotkey("alt", "f")
 gui.sleep(1)
-gui.moveTo(692, 546, duration=1)
+gui.moveTo(692, 546)
 gui.doubleClick()
 gui.sleep(1)
 
-target_file_name = 'trial_balance' + target_date
+target_file_name = "trial_balance" + target_date
 gui.write(target_file_name, interval=0.4)
 gui.sleep(2)
-gui.moveTo(1005, 547, duration=1)
+
+gui.moveTo(1005, 547)
 gui.click()
 gui.sleep(2)
 
@@ -80,17 +86,19 @@ gui.click()
 
 gui.sleep(3)
 
-# 자료를 다운로드 받는 down디렉토리 생성
-down_base_dir = './downdata/' + target_date
+# 자료를 다운로드 받는 down디렉토리 확인 (및 생성)
+down_base_dir = "./downdata/" + target_date
 try:
     if os.path.exists(down_base_dir) == False:
         os.makedirs(down_base_dir)
 except:
-    with open('./error.log', 'a') as file:
-        file.write(f'[down_opera.py] : {datetime.datetime.now()} - Exception: Cannot create the directory {down_base_dir}')
+    with open("./error.log", "a") as file:
+        file.write(
+            f"[down_opera.py] : {datetime.datetime.now()} - Exception: Cannot create the directory {down_base_dir}"
+        )
         print("Error: Cannot create the directory {}".format(down_base_dir))
 
 # 파일 옮기기 : downdata디렉토리로
 # 디렉토리의 파일만 검색해서 .. 파일 이름에서 유추... 해당 파일을 복사/이동
-target_file_name = 'C:/users/fa2/' + target_file_name + '.xml'
+target_file_name = "C:/users/fa2/" + target_file_name + ".xml"
 shutil.move(target_file_name, down_base_dir)
