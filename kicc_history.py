7f7998f0bd8e5c7e37efc05f8f22faaf30aed918
talhea <talhea.seo@gmail.com>
2022-07-10 07:58:16 +0900

    kicc_history 중복 제거 수정

--- kicc_history.py
+++ kicc_history.py
@@ -27,19 +27,17 @@ def to_card_history_df():
     # 2-2. '거래고유번호' 컬럼에 있는 내용중에 NaN인 행 제거
     card_history_df = card_history_df.dropna(subset=['거래고유번호'])       # '거래고유번호'가 NaN 값이면 빈칸이거나 잘못된 라인
 
-    # 2-3. '승인번호' 중복 라인제거를 위한 중복 정보 추출
-    dup_series = card_history_df.duplicated(['승인번호'], keep=False)       # '승인번호' 중복 여부 Bool series 리턴, 중복되면 모두 True
-    card_history_df = pnds.concat([card_history_df, dup_series], axis=1)    # 위 반환된 결과를 dataframe column 으로 추가
-    card_history_df.rename(columns={0: 'dup'}, inplace=True)                # 추가된 중복 컬럼이름을 'dup'로 변경해서 조건식때 사용
+    # 2-3. '승인번호' 중복 라인제거 == 당일 취소 건 제거
+    card_history_df.drop_duplicates(subset='승인번호', keep=False, inplace=True)    # keep= 중복 라인들 모두 제거
 
-    # 2-4. '승인번호' 중복 라인들을 제외하면서, 동시에 필요 컬럼들만 추출
-    card_history_df = card_history_df.loc[card_history_df['dup'] == False][['거래고유번호', '승인구분','date',
-                                                                            '카드번호', '발급카드사', '매입카드사', '금액']]
+    # 2-4. 
+    card_history_df = card_history_df[['거래고유번호', '승인구분', 'date', '승인번호',
+                                        '카드번호', '발급카드사', '매입카드사', '금액']]
 
     # 2-5. date 기준 sorting
     card_history_df = card_history_df.sort_values(by=['date'])
 
-    # 2-6. date 포맷 변경: '%Y-%m-%d'
+    # 2-6. date 정렬이후 포맷 변경: '%Y-%m-%d'
     card_history_df['date'] = card_history_df['date'].map(lambda str_data: str_data.split()[0], na_action='ignore') # 날짜 포맷 : '%Y-%m-%d'
 
     # 3. dt디렉토리에 dataframe 저장
